ef28c13ce7a78527d790ba72786bd71307a2923e
Vladislavs <96728187+Otbivnushe4ka@users.noreply.github.com>
2023-09-25 10:26:36 +0300

    storage update

--- routes/storage.js
+++ routes/storage.js
@@ -1,11 +1,30 @@
 const { Router } = require("express");
 const route = Router();
+const db = require("../db");
 
-route.get("/storage", (req, res) => {
-  res.render("tables/storage", {
-    page: "storage",
-    title: "Storage",
-  });
+route.get("/storage", async(req, res) => {
+	const products = await db.getData(`
+    SELECT 
+    storage.id, 
+    storage.name AS 'name', 
+    storage.cost, 
+    storage.quantity, 
+    storage.serial_num, 
+    codes.barcode, 
+    products_tips.name AS 'type'
+    
+    FROM storage 
+    LEFT JOIN codes 
+    ON storage.id = codes.id
+    LEFT JOIN products_tips 
+    ON products_tips.id = storage.product_tip`);
+
+     console.log(products);
+	res.render("tables/storage", {
+		page: "storage",
+		title: "Storage",
+        products: products,
+	});
 });
 
 module.exports = route;
