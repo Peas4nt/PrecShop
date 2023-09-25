2e6d737fc20e2d3a1a3096c14092167b272628dd
zer0conf <red@litesec.co>
2023-09-25 11:01:30 +0300

    Types

--- routes/signup.js
+++ routes/signup.js
@@ -3,35 +3,36 @@ const route = Router();
 const db = require("../db");
 
 route.get("/signup", (req, res) => {
-	res.render("login/signup", {
-		page: "signup",
-		title: "signup",
-	});
+  res.render("login/signup", {
+    page: "signup",
+    title: "signup",
+  });
 });
 
 route.post("/signup", async (req, res) => {
-	const name = req.body.name;
-	const lastname = req.body.lastname;
-	const email = req.body.email;
-	const password = req.body.password;
+  const name = req.body.name;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  const password = req.body.password;
 
-	await db
-		.insertData(
-			"INSERT INTO users(name,lastname,email,password) VALUES (?, ?, ?, ?)",
-			[name, lastname, email, password],
-		)
-		.then((result) => {
-			if (result) {
-				console.log(`User ${name} was created`);
-				res.status(200).redirect("/");
-			} else {
-				console.log(`User ${name} was not created`);
-				res.status(500).redirect("/signup");
-			}
-		}).catch((error) => {
-			console.log("Create user: ", error)
-			res.status(500).send("Server error");
-		});
+  await db
+    .insertData(
+      "INSERT INTO users(name,lastname,email,password) VALUES (?, ?, ?, ?)",
+      [name, lastname, email, password]
+    )
+    .then((result) => {
+      if (result) {
+        console.log(`User ${name} was created`);
+        res.status(200).redirect("/");
+      } else {
+        console.log(`User ${name} was not created`);
+        res.status(500).redirect("/signup");
+      }
+    })
+    .catch((error) => {
+      console.log("Create user: ", error);
+      res.status(500).send("Server error");
+    });
 });
 
 module.exports = route;
