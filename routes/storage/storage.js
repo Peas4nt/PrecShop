58822db9fb2149970bde53b6572d14efa00bc1be
Vladislavs <96728187+Otbivnushe4ka@users.noreply.github.com>
2023-10-07 14:12:26 +0300

    add a pagination for storage

--- routes/storage/storage.js
+++ routes/storage/storage.js
@@ -2,7 +2,13 @@ const { Router } = require("express");
 const route = Router();
 const db = require("../../db");
 
-route.get("/storage", async (req, res) => {
+route.get("/storage/:page?", async (req, res) => {
+    const currentPage = req.params.page || 1;
+    const productsPerPage = 2;
+    // array indexes
+    const startIndex = (currentPage - 1) * productsPerPage;
+    const endIndex = startIndex + productsPerPage;
+
 	const products = await db.getData(`
     SELECT 
     storage.id, 
@@ -21,14 +27,28 @@ route.get("/storage", async (req, res) => {
         ON products_tips.id = storage.product_tip
     LEFT JOIN users
         ON storage.user_id = users.id
-    `);
+    `).catch(err => {
+        console.log("error: ", error);
+        res.status(500).send("Server error");
+    });
 
+    const productMaxPages = products.length / productsPerPage;
+    const productsToShow = products.slice(startIndex, endIndex)
 	// console.log(products);
-	res.render("tables/storage", {
-		page: "storage",
-		title: "Storage",
-		products: products,
-	});
+    if (currentPage > productMaxPages || currentPage < 1) {
+        res.status(404).send("That page is out of the allowed")
+    } else {
+        res.render("tables/storage", {
+            page: "storage",
+            title: "Storage",
+            products: productsToShow,
+            pagination: {
+                currentPage,
+                productMaxPages
+            }
+        });
+    }
+
 });
 
 module.exports = route;
