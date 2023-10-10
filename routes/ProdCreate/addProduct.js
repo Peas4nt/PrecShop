2ed694051fdbec0518e25233f0b95d7acf1a00c0
BobabBaobab <mlisnojs@gmail.com>
2023-10-10 11:07:14 +0300

    da

--- routes/ProdCreate/addProduct.js
+++ routes/ProdCreate/addProduct.js
@@ -3,11 +3,11 @@ const route = Router();
 const db = require("../../db");
 
 route.get("/add/product/:barcode", async (req, res) => {
-	const code = req.params.barcode;
+  const code = req.params.barcode;
 
-	await db
-		.getData(
-			`
+  await db
+    .getData(
+      `
   SELECT storage.id, storage.name, storage.cost, storage.quantity, storage.serial_num, products_tips.name AS tip
   FROM storage
   LEFT JOIN codes 
@@ -15,29 +15,38 @@ route.get("/add/product/:barcode", async (req, res) => {
   LEFT JOIN products_tips
   ON products_tips.id = storage.product_tip
   WHERE codes.barcode = '${code}';
-  `,
-		)
-		.then((result) => {
-			if (result.length === 0) {
-				throw new Error("That code is not available");
-			}
-			res.render("create product/product_add", {
-				page: "product_create",
-				title: "Add a product",
-				barcode: code,
-				product: result[0],
-			});
-		})
-		.catch((err) => {
-			res.status(500).send(err.message);
-		});
+  `
+    )
+    .then((result) => {
+      if (result.length === 0) {
+        throw new Error("That code is not available");
+      }
+      res.render("create product/product_add", {
+        page: "product_create",
+        title: "Add a product",
+        barcode: code,
+        product: result[0],
+      });
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
 });
 
 route.post("/add/product", async (req, res) => {
-    // product_id
-    // import_date'
-    // quantity
-
+  const product_id = req.body.product_id;
+  const import_date = req.body.import_date;
+  const quantity = req.body.quantity;
+  console.log(product_id);
+  
+  await db.getData
+(
+	`
+	UPDATE storage 
+	SET quantity = quantity + ${quantity}
+	WHERE id = ${product_id}`
+)
+res.redirect(`/storage/product/${product_id}`)
 });
 
 module.exports = route;
--- routes/tables/importedTable.js
+++ routes/tables/importedTable.js
@@ -1,10 +1,40 @@
 const { Router } = require("express");
 const route = Router();
+const db = require("../../db")
 
-route.get("/tables/importedProducts", (req, res) => {
+route.get("/tables/importedProducts/:page?", async (req, res) => {
+  const products = await db.getData(`
+  SELECT 
+  storage.name AS "name",
+  users.name AS "importer",
+  imported_products.quantity as quantity,
+  DATE_FORMAT(delivery_date, "%d/%m/%Y") AS "deliverydate"
+  FROM imported_products 
+  LEFT JOIN storage 
+  ON imported_products.product_id = storage.id
+  LEFT JOIN users
+  ON imported_products.user_id = users.id
+  ORDER BY delivery_date DESC`
+  );
+    
+  const productsPerPage = 10;
+  const productMaxPages = Math.ceil(products.length / productsPerPage);
+  // current page
+  const currentPage = (!req.params.page || req.params.page > productMaxPages || req.params.page < 1)? 1 : req.params.page;
+
+  // array indexes
+  const startIndex = (currentPage - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
+  const productsToShow = products.slice(startIndex, endIndex)
+
   res.render("tables/importedProducts", {
     page: "importedProducts",
     title: "imported Products",
+    imported_products: productsToShow,
+    pagination: {
+      currentPage,
+      productMaxPages
+    }
   });
 });
 
