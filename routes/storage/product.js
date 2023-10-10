d324a9feb85328545e357796aa0b3b0caf306877
Vladislavs <96728187+Otbivnushe4ka@users.noreply.github.com>
2023-10-10 12:19:27 +0300

    product new update

--- routes/storage/product.js
+++ routes/storage/product.js
@@ -3,9 +3,9 @@ const route = Router();
 const db = require("../../db");
 
 route.get("/storage/product/:id?", async (req, res) => {
-    const prodId = req.params.id ?? 1;
+	const prodId = req.params.id ?? 1;
 
-    const product = await db.getData(`
+	const product = await db.getData(`
     SELECT 
     storage.id, 
     storage.name AS 'name', 
@@ -24,17 +24,77 @@ route.get("/storage/product/:id?", async (req, res) => {
     LEFT JOIN users
         ON storage.user_id = users.id
    	WHERE storage.id=${prodId}
-    `)
-    
+    `);
 
-    res.render("storage/product", {
-        page: "product",
-        title: "Product",
-        prod: product[0]
-    })
-})
+	const exported_products = await db.getData(`
+    SELECT
+    storage.name AS "name",
+    users.name AS "exporter",
+    exported_products.quantity as quantity,
+    DATE_FORMAT(remove_date, "%d/%m/%Y") AS "removedate"
+    FROM exported_products 
+    LEFT JOIN storage 
+    ON exported_products.product_id = storage.id
+    LEFT JOIN users
+    ON exported_products.user_id = users.id
+    WHERE storage.id = ${prodId}
+    ORDER BY remove_date DESC`);
 
-route.get("/storage/product/", (req, res) => {
+	const imported_products = await db.getData(`
+  SELECT 
+  storage.name AS "name",
+  users.name AS "importer",
+  imported_products.quantity as quantity,
+  DATE_FORMAT(delivery_date, "%d/%m/%Y") AS "deliverydate"
+  FROM imported_products 
+  LEFT JOIN storage 
+  ON imported_products.product_id = storage.id
+  LEFT JOIN users
+  ON imported_products.user_id = users.id
+  WHERE storage.id = ${prodId}
+  ORDER BY delivery_date DESC`);
 
+	res.render("storage/product", {
+		page: "product",
+		title: "Product",
+		prod: product[0],
+		exported_products,
+		imported_products,
+	});
 });
+
+route.post("/export/product/", async (req, res) => {
+	const quantity = req.body.quantity;
+	const date = req.body.date;
+	const prodId = req.body.prod_id;
+	const userId = 1;
+
+	db.getData(
+		`
+    UPDATE storage 
+	SET quantity = quantity - ${quantity}
+	WHERE id = ${prodId}
+
+    `,
+	).catch((err) => {
+		console.log("error: ", error);
+		res.status(500).send("Server error");
+	});
+
+	db.insertData(
+		`INSERT INTO exported_products 
+    (quantity,product_id,user_id,remove_date) 
+    VALUES (?,?,?,?)`,
+		[quantity, prodId, userId, date],
+	).catch((err) => {
+		console.log("error: ", error);
+		res.status(500).send("Server error");
+	});
+
+	res.redirect(`/storage/product/${prodId}`);
+});
+
+route.put("/edit/product", (req, res) => {});
+route.delete("/delete/product", (req, res) => {});
+
 module.exports = route;
