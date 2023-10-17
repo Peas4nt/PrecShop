ddc80fa8bb7a9a5ba58b1105d43964eeb5d2c8e5
BobabBaobab <mlisnojs@gmail.com>
2023-10-17 09:59:04 +0300

    m.a.k.s.i.m. update

--- routes/storage/storage.js
+++ routes/storage/storage.js
@@ -2,8 +2,16 @@ const { Router } = require("express");
 const route = Router();
 const db = require("../../db");
 
-route.get("/storage/:page?", async (req, res) => {
-	const products = await db.getData(`
+route.get("/storage/page/:page?", async (req, res) => {
+  const search = req.body.search;
+
+  console.log(search);
+  let  products = [];
+  if (search) {
+    console.log("Ne Pusto");
+    products = await db
+      .getData(
+        `
     SELECT 
     storage.id, 
     storage.name AS 'name', 
@@ -21,34 +29,68 @@ route.get("/storage/:page?", async (req, res) => {
         ON products_tips.id = storage.product_tip
     LEFT JOIN users
         ON storage.user_id = users.id
-    `).catch(error => {
+    WHERE name LIKE '%${search}%'
+    `
+      )
+      .catch((error) => {
         console.log("error: ", error);
         res.status(500).send("Server error");
-    });
+      });
+  } else {
+    products = await db
+      .getData(
+        `
+    SELECT 
+    storage.id, 
+    storage.name AS 'name', 
+    storage.cost, 
+    storage.quantity, 
+    storage.serial_num, 
+    codes.barcode, 
+    products_tips.name AS 'type',
+    users.name AS 'user'
+    
+    FROM storage 
+    LEFT JOIN codes 
+        ON storage.code_id = codes.id
+    LEFT JOIN products_tips 
+        ON products_tips.id = storage.product_tip
+    LEFT JOIN users
+        ON storage.user_id = users.id
 
+    `
+      )
+      .catch((error) => {
+        console.log("error: ", error);
+        res.status(500).send("Server error");
+      });
+    console.log("Pusto");
+  }
 
-    const productsPerPage = 5;
-    const productMaxPages = Math.ceil(products.length / productsPerPage);
-    // current page
-    const currentPage = (!req.params.page || req.params.page > productMaxPages || req.params.page < 1)? 1 : req.params.page;
+  const productsPerPage = 5;
+  const productMaxPages = Math.ceil(products.length / productsPerPage);
+  // current page
+  const currentPage =
+    !req.params.page || req.params.page > productMaxPages || req.params.page < 1
+      ? 1
+      : req.params.page;
 
-    // array indexes
-    const startIndex = (currentPage - 1) * productsPerPage;
-    const endIndex = startIndex + productsPerPage;
-    const productsToShow = products.slice(startIndex, endIndex)
+  // array indexes
+  const startIndex = (currentPage - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
+  const productsToShow = products.slice(startIndex, endIndex);
 
-	// console.log(productMaxPages);
+  // console.log(productMaxPages);
 
-        res.render("storage/storage", {
-            page: "storage",
-            title: "Storage",
-            products: productsToShow,
-            pagination: {
-                currentPage,
-                productMaxPages
-            }
-        });
-
+  res.render("storage/storage", {
+    page: "storage",
+    title: "Storage",
+    products: productsToShow,
+    pagination: {
+      currentPage,
+      productMaxPages,
+    },
+  });
 });
 
 module.exports = route;
--- routes/userLog/login.js
+++ routes/userLog/login.js
@@ -1,7 +1,32 @@
 const { Router } = require("express");
+const db = require("../../db");
 const route = Router();
 
-route.get("/login", (req, res) => {
+route.post("/login", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(email,password);
+  await db
+    .getData(
+      "SELECT EXISTS(SELECT * FROM users WHERE email = '" +
+				email +
+				"' AND password = '" +
+				password +
+				"') as s",
+    )
+    .then((result) => {
+			if (result[0].s === 1) {
+        
+          console.log("1");
+          res.json(1);
+			} else {
+          console.log("0");
+          res.json("Wrong email or password");
+			}
+		})
+})
+route.get("/login",(req, res) => {
+
   res.render("login/login", {
     page: "login",
     title: "login",
