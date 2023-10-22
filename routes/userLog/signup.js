aa3529facb865793e82acad021f2cdf440688545
Vladislavs <96728187+Otbivnushe4ka@users.noreply.github.com>
2023-10-23 00:12:02 +0300

    add singup check

--- routes/userLog/signup.js
+++ routes/userLog/signup.js
@@ -16,20 +16,36 @@ route.post("/signup", async (req, res) => {
 	const email = req.body.email;
 	const password = req.body.password;
 
+	await db
+		.getData(
+			"SELECT EXISTS(SELECT * FROM codes WHERE barcode = '" +
+				code +
+				"' ) as s",
+		)
+		.then((result) => {
+			if (result[0].s === 1) {
+				res.status(400).json("User already exists");
+			}
+		})
+		.catch((err) => {
+			res.status(500).json("User was not created. Please try again.");
+		});
 
 	await db
 		.insertData(
 			"INSERT INTO users(name,lastname,email,password) VALUES (?, ?, ?, ?)",
-			[name, lastname, email, password]
+			[name, lastname, email, password],
 		)
 		.then((result) => {
 			console.log(result);
 			if (result.result) {
-				
 				console.log(`User ${name} was created`);
 				req.session.user = {
-					id: result.id, name, lastname, email
-				}
+					id: result.id,
+					name,
+					lastname,
+					email,
+				};
 				res.status(200).json(1);
 			} else {
 				console.log(`User ${name} was not created`);
