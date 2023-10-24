ad4a80eff48c039697c4967daadf47a490318064
BobabBaobab <mlisnojs@gmail.com>
2023-10-24 11:53:21 +0300

    settigs

--- routes/profile/settings.js
+++ routes/profile/settings.js
@@ -1,13 +1,37 @@
-const {
-	Router
-} = require("express");
+const { Router } = require("express");
 const route = Router();
 const db = require("../../db");
 const { checkAuthentication } = require("../../modules/checklogin");
 
-route.get("/settings", checkAuthentication, (req, res) => {
+route.get("/settings/", checkAuthentication, async (req, res) => {
+  const userid = req.session.user.id;
+  const user = await db.getData(`SELECT * FROM users WHERE id = ${userid}`);
 
+  res.render("profile/settings", {
+    session: userid,
+    page: "settings",
+    title: "Settings",
+    user: user[0],
+  });
 });
 
+// route.post("/settings/", checkAuthentication, async (req, res) => {
+//   const userid = req.session.user.id;
+//   const name = req.body.name;
+//   const lastname = req.body.lastname;
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   console.log(name,lastname,email,password)
+//   await db.insertData(
+//     `UPDATE users
+// 		SET
+// 		name = ${name},
+// 		lastname = ${lastname},
+// 		email = ${email},
+// 		password = ${password}
+// 		WHERE id = ${userid}
+// 		`
+//   );
+// });
 
 module.exports = route;
