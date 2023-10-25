ea44aa55bcb23ad7756ade75d19c9870d6c475a1
BobabBaobab <mlisnojs@gmail.com>
2023-10-25 10:38:04 +0300

    Delete

--- routes/storage/product.js
+++ routes/storage/product.js
@@ -32,6 +32,7 @@ route.get("/storage/product/:id?", async (req, res) => {
     storage.name AS "name",
     users.name AS "exporter",
     exported_products.quantity as quantity,
+    exported_products.object as object,
     DATE_FORMAT(remove_date, "%d/%m/%Y") AS "removedate"
     FROM exported_products 
     LEFT JOIN storage 
@@ -67,8 +68,10 @@ route.get("/storage/product/:id?", async (req, res) => {
 
 route.post("/export/product/", checkAuthentication, async (req, res) => {
   const quantity = req.body.quantity;
+  const object = req.body.object;
   const date = req.body.date;
   const prodId = req.body.prod_id;
+
   const userId = req.session.user.id;
 
   db.getData(
@@ -85,9 +88,9 @@ route.post("/export/product/", checkAuthentication, async (req, res) => {
 
   db.insertData(
     `INSERT INTO exported_products 
-    (quantity,product_id,user_id,remove_date) 
-    VALUES (?,?,?,?)`,
-    [quantity, prodId, userId, date]
+    (quantity,object,product_id,user_id,remove_date) 
+    VALUES (?,?,?,?,?)`,
+    [quantity, object, prodId, userId, date]
   ).catch((error) => {
     console.log("error: ", error);
     res.status(500).send("Server error");
@@ -98,10 +101,19 @@ route.post("/export/product/", checkAuthentication, async (req, res) => {
 
 route.put("/edit/product", checkAuthentication, (req, res) => {});
 
-route.delete("/delete/product", checkAuthentication, (req, res) => {
+route.delete("/delete/product", checkAuthentication, async (req, res) => {
   const prodId = req.body.id;
-  db.getData(`DELETE FROM storage WHERE id = ${prodId}`)
+  barcode_id = await db.getData(
+    `SELECT code_id FROM storage WHERE id = ${prodId}`
+  );
+  
+  console.log(barcode_id[0]);
+  await db
+    .getData(`DELETE FROM storage WHERE id = ${prodId}`)
     .then(() => {
+      db.getData(`DELETE FROM exported_products WHERE product_id = ${prodId}`);
+      db.getData(`DELETE FROM imported_products WHERE product_id = ${prodId}`);
+      db.getData(`DELETE FROM codes WHERE id=${barcode_id[0].code_id}`);
       res.status(200).json("1");
       console.log("Deleted");
     })
